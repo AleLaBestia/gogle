a6eadef26e19aa0315282324917d74d11c2fecf2
marcelo alejnadro herlan <alelabestiadekiev@gmail.com>
2021-05-31 00:56:26 +0300

    first commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Anna\\Desktop\\youtube\\youtube-tutorials-repo-master\\5.google-stop-trigger\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-jsx": hot(preferDefault(require("C:\\Users\\Anna\\Desktop\\youtube\\youtube-tutorials-repo-master\\5.google-stop-trigger\\src\\pages\\index.jsx")))
+}
+
